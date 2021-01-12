dde151982b7be60001d8a09c73a9f531842f930f
Mohammed Saeed Alowa <47119751+mohsowa@users.noreply.github.com>
2021-01-12 20:31:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // Created by MOHSOWA
-'use strict';
+"use strict";
 
 // Player 1
 let player_1_score = 0;
 let player_1_totalScore = 0;
-const displayScore_1 = document.querySelector('#score--0');
-const displaycurrent_1 = document.querySelector('#current--0');
-const ActivePlayer_1 = document.querySelector('.player--0');
+const displayScore_1 = document.querySelector("#score--0");
+const displaycurrent_1 = document.querySelector("#current--0");
+const ActivePlayer_1 = document.querySelector(".player--0");
 
 // Player 2
 let player_2_score = 0;
 let player_2_totalScore = 0;
-const displayScore_2 = document.querySelector('#score--1');
-const displaycurrent_2 = document.querySelector('#current--1');
-const ActivePlayer_2 = document.querySelector('.player--1');
+const displayScore_2 = document.querySelector("#score--1");
+const displaycurrent_2 = document.querySelector("#current--1");
+const ActivePlayer_2 = document.querySelector(".player--1");
 
 // main Player
 let main_player = 1;
@@ -21,7 +21,7 @@ let main_player = 1;
 let game_finish = false;
 
 // dice
-const dise = document.querySelector('.dice');
+const dise = document.querySelector(".dice");
 
 //// main function
 const ressit_score = function () {
@@ -36,17 +36,17 @@ const ressit_score = function () {
   displayScore_2.textContent = player_2_score;
   displaycurrent_2.textContent = 0;
 
-  dise.classList.add('hidden');
+  dise.classList.add("hidden");
 
   main_player = 1;
-  if (ActivePlayer_1.classList.contains('player--winner')) {
-    ActivePlayer_1.classList.remove('player--winner');
-  } else if (ActivePlayer_2.classList.contains('player--winner')) {
-    ActivePlayer_2.classList.remove('player--winner');
-  } else if (ActivePlayer_2.classList.contains('player--active')) {
-    ActivePlayer_2.classList.remove('player--active');
-  } else if (!ActivePlayer_1.classList.contains('player--active')) {
-    ActivePlayer_1.classList.add('player--active');
+  if (ActivePlayer_1.classList.contains("player--winner")) {
+    ActivePlayer_1.classList.remove("player--winner");
+  } else if (ActivePlayer_2.classList.contains("player--winner")) {
+    ActivePlayer_2.classList.remove("player--winner");
+  } else if (ActivePlayer_2.classList.contains("player--active")) {
+    ActivePlayer_2.classList.remove("player--active");
+  } else if (!ActivePlayer_1.classList.contains("player--active")) {
+    ActivePlayer_1.classList.add("player--active");
   }
 };
 ressit_score();
@@ -55,7 +55,7 @@ const dice_roll = function () {
   if (!game_finish) {
     const dice_roll_number = Number(Math.trunc(Math.random() * 6) + 1); // random number between 1-6
     dise.src = `dice-${dice_roll_number}.png`; // change image source
-    dise.classList.remove('hidden'); // show image
+    dise.classList.remove("hidden"); // show image
 
     if (dice_roll_number === 1) {
       // switch player
@@ -79,12 +79,12 @@ const swith_Player = function () {
   displaycurrent_1.textContent = displaycurrent_2.textContent = 0;
   if (main_player === 1) {
     main_player = 2;
-    ActivePlayer_1.classList.remove('player--active');
-    ActivePlayer_2.classList.add('player--active');
+    ActivePlayer_1.classList.remove("player--active");
+    ActivePlayer_2.classList.add("player--active");
   } else {
     main_player = 1;
-    ActivePlayer_2.classList.remove('player--active');
-    ActivePlayer_1.classList.add('player--active');
+    ActivePlayer_2.classList.remove("player--active");
+    ActivePlayer_1.classList.add("player--active");
   }
 };
 
@@ -105,29 +105,29 @@ const hold = function () {
 
 // win in game
 const win_player = function () {
-  if ((player_1_score >= 20) & (main_player === 1)) {
-    console.log('player 1 win');
-    ActivePlayer_1.classList.remove('player--active');
-    ActivePlayer_1.classList.add('player--winner');
+  if ((player_1_score >= 100) & (main_player === 1)) {
+    console.log("player 1 win");
+    ActivePlayer_1.classList.remove("player--active");
+    ActivePlayer_1.classList.add("player--winner");
     game_finish = true;
-  } else if ((player_2_score >= 20) & (main_player === 2)) {
-    console.log('player 2 win');
-    ActivePlayer_2.classList.remove('player--active');
-    ActivePlayer_2.classList.add('player--winner');
+  } else if ((player_2_score >= 100) & (main_player === 2)) {
+    console.log("player 2 win");
+    ActivePlayer_2.classList.remove("player--active");
+    ActivePlayer_2.classList.add("player--winner");
     game_finish = true;
   }
 };
 
 // New Game btn
-const new_game = document.querySelector('.btn--new');
-new_game.addEventListener('click', function () {
+const new_game = document.querySelector(".btn--new");
+new_game.addEventListener("click", function () {
   ressit_score();
 });
 
 // Role Dice
-const rolling_btn = document.querySelector('.btn--roll');
-rolling_btn.addEventListener('click', dice_roll);
+const rolling_btn = document.querySelector(".btn--roll");
+rolling_btn.addEventListener("click", dice_roll);
 
 // Hold btn
-const hold_score = document.querySelector('.btn--hold');
-hold_score.addEventListener('click', hold);
+const hold_score = document.querySelector(".btn--hold");
+hold_score.addEventListener("click", hold);
